d57af26abeb70711524c4e1e1b4e3095a1992999
Miku2906 <112625197+Miku2906@users.noreply.github.com>
2022-09-02 23:27:28 +0700

    Add files via upload

---
+++ Javascript/Script1.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+$(document).ready(function () { 
+
+    const btn1 = document.querySelector("#btn1");
+    btn1.addEventListener("click", function () {
+        window.scrollTo(0, 2500);
+    });
+    const btn2 = document.querySelector("#btn2");
+    btn2.addEventListener("click", function () {
+        window.scrollTo(0, 3200);
+    });
+
+});
